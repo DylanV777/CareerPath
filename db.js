4c1eeca8af4a5b9048511cd83a91cc3287cd5142
Nova-Alejandro <david.alejandro.bn.097@gmail.com>
2026-07-16 09:55:06 -0500

    Update db.js to use dotenv for configuration
    Refactor database connection to use environment variables.

--- db.js
+++ db.js
@@ -1,6 +1,15 @@
-DB_HOST=localhost
-DB_PORT=5432
-DB_USER=postgres
-DB_PASSWORD=12345
-DB_NAME=deiby
-port=3000
+//Importa las librerias de dotenv y pg
+
+import 'dotenv/config';
+import pg from 'pg';
+const { Pool } = pg;   
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+export default pool;
